74ebb7b4897dd1c2812e48b46f949e4d387197f9
asheekaa2008 <72641984+asheekaa2008@users.noreply.github.com>
2021-10-30 08:11:33 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,11 @@
+class Atm:
+    def __init__( CashWithdrawl, BalanceEnquiry):
+        self.CashWithdrawl=CashWithdrawl
+        self.BalanceEnquiry=BalanceEnquiry
+    def withdrawl(self):
+        print("i want to withdraw cash")
+    def balanceenquiry(self):
+        print("i want to enquire about my bank account")
+Axsis=Atm("1000","10000")
+#Axsis.withdrawl
+print(Atm.model)
